13ce0da8ea176061f13847180855c604a0d7de92
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:01:54 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'Music',
-  icon: 'tabler:music',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Storage'
 } satisfies ModuleConfig
